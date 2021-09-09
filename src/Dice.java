8b1d74d06128ebb5818c944b38f31f8b66a1835d
williamProgrammerar <70644042+williamProgrammerar@users.noreply.github.com>
2021-09-09 15:31:13 +0200

    Added runnable dice class

---
+++ src/Dice.java
@@ -0,0 +1,26 @@
+import java.util.Random;
+
+public class Dice {
+    Random rand = new Random();
+    private int dice1;
+    private int dice2;
+
+    public void rollDice() {
+        dice1 = rand.nextInt(6) + 1;
+        dice2 = rand.nextInt(6) + 1;
+    }
+
+    public int getDice1() {
+        return dice1;
+    }
+
+    public int getDice2() {
+        return dice2;
+    }
+
+    public int getSum() {
+        return dice1 + dice2;
+    }
+}
+
+
--- src/Monopoly.java
+++ src/Monopoly.java
@@ -2,8 +2,16 @@ import javafx.application.Application;
 import javafx.stage.Stage;
 
 public class Monopoly extends Application {
+    private Dice dice = new Dice();
+
     @Override
     public void start(Stage stage) throws Exception {
         System.out.println("test");
+        for (int i = 0; i < 10; i++) {
+            dice.rollDice();
+            System.out.println( "Dice 1: " + dice.getDice1() );
+            System.out.println( "Dice 2: " + dice.getDice2() );
+            System.out.println( "Sum of dice throws: " + dice.getSum() );
+        }
     }
 }
