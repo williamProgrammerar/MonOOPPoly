e89d0c498f2170cdffb9ee9bd1217e3105d73f79
rhedinh <hampusrhedinstam@gmail.com>
2021-09-15 11:53:00 +0200

    Huvudklass

--- src/Model.java
+++ src/Model.java
@@ -1,4 +1,5 @@
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.shape.Circle;
 import javafx.scene.text.Text;
 
@@ -11,9 +12,17 @@ public class Model {
     @FXML
     private Circle playerPiece;
 
+    @FXML
+    Button dice1;
+
+    @FXML
+    Button dice2;
+
     public void rollDice() {
         dice.rollDice();
         diceText.setText("The sum of the two dices rolled is: " + dice.getSum());
-        playerPiece.setCenterX(playerPiece.getCenterX() + (dice.getSum() * 10));
+        playerPiece.setCenterX(playerPiece.getCenterX() - (dice.getSum() * 10));
+        dice1.setText(String.valueOf(dice.getDice1()));
+        dice2.setText(String.valueOf(dice.getDice2()));
     }
 }
