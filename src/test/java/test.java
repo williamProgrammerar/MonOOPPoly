f786e1ef07e1a7720c2ec64e9e31b44e35089aa7
williamProgrammerar <70644042+williamProgrammerar@users.noreply.github.com>
2021-09-21 19:08:57 +0200

    Tests for changing the current player

--- src/test/java/test.java
+++ src/test/java/test.java
@@ -1,8 +1,30 @@
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class test {
+    List<String> list = new ArrayList<>();
+
+    @BeforeEach
+    void setUp() {
+        list.add("William");
+        list.add("Hampus");
+        list.add("Jon");
+        list.add("Vilhelm");
+    }
+
     @Test
-    void demoTestMethod() {
-        System.out.println("hej");
+    void testChangePlayer() {
+        for (int r = 0; r < 10; r++) {
+            for (int i = 0; i < list.size(); i++) {
+                System.out.println(list.get(i));
+            }
+            String tmp = list.get(0);
+            list.remove(0);
+            list.add(tmp);
+            System.out.println("");
+        }
     }
 }
