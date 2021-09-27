66499b815331bc75affa96542c1d4a78dd65b833
JonEmilsson <jon.emilsson0522@gmail.com>
2021-09-27 15:54:53 +0200

    Fixed method names and added exitGame

--- src/main/java/Controller/menuController.java
+++ src/main/java/Controller/menuController.java
@@ -16,11 +16,14 @@ public class menuController {
     @FXML private Button startGameButton;
 
     @FXML
-    public void blarg(ActionEvent event) throws IOException {
+    public void startGame(ActionEvent event) throws IOException {
         Parent monopolyParent = FXMLLoader.load(getClass().getResource("/fxml/ChalmersMonopoly.fxml"));
         Scene monopolyScene = new Scene(monopolyParent);
         Stage window = (Stage)((Node) event.getSource()).getScene().getWindow();
         window.setScene(monopolyScene);
         window.show();
+    }
+    @FXML public void exitGame(ActionEvent event){
+        System.exit(0);
     }
 }
