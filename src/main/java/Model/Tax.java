a3a0d3e34f21fab2dc3f7ba76bc86bd8f7a6225e
williamProgrammerar <70644042+williamProgrammerar@users.noreply.github.com>
2021-09-27 09:45:50 +0200

    Added Tax class

---
+++ src/main/java/Model/Tax.java
@@ -0,0 +1,15 @@
+package Model;
+
+public class Tax extends Space {
+
+    private final int tax;
+
+    public Tax(String spaceName, int tax) {
+        super(spaceName);
+        this.tax = tax;
+    }
+
+    public int getTax() {
+        return tax;
+    }
+}
