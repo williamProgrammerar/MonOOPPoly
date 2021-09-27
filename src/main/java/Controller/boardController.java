fa5e6033c3bf01531b586158336c545e98a2a668
rhedinh <rhedinh@student.chalmers.se>
2021-09-27 17:01:31 +0200

    Created grid and spaces for GUI
    Now there is spaces in the GUI following a grid.

--- src/main/java/Controller/boardController.java
+++ src/main/java/Controller/boardController.java
@@ -1,28 +1,43 @@
 package Controller;
 
+import Model.Board;
 import Model.Game;
+import Model.Space;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.layout.GridPane;
 
-public class boardController {
-        Game game = new Game();
-        // everything involving controlling the dice should be moved here and removed from the Game class.
-        @FXML
-        Button dice1;
-        @FXML
-        Button dice2;
-        @FXML
-        ImageView piece = new ImageView(new Image("pic/Asmurf-icon.png"));
+import java.net.URL;
+import java.util.ResourceBundle;
 
-    public boardController() throws Exception {
+public class boardController implements Initializable {
+    //Game game = new Game();
+    Board board = new Board();
+    // everything involving controlling the dice should be moved here and removed from the Game class.
+    @FXML
+    Button dice1;
+    @FXML
+    Button dice2;
+    @FXML
+    ImageView piece = new ImageView(new Image("pic/Asmurf-icon.png"));
+    @FXML
+    GridPane boardGrid;
+    @FXML
+    spaceController space1Controller;
+    @FXML spaceController space2Controller;
+
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        space1Controller.hejText.setText(board.getSpace(0).getSpaceName());
     }
 
     public void rollDice() {
-            game.dice.rollDice();
-            piece.setX(piece.getX() + game.dice.getSum()*10);
-            dice1.setText(String.valueOf(game.dice.getDice1()));
-            dice2.setText(String.valueOf(game.dice.getDice2()));
-        }
+        /*game.dice.rollDice();
+        piece.setX(piece.getX() + game.dice.getSum() * 10);
+        dice1.setText(String.valueOf(game.dice.getDice1()));
+        dice2.setText(String.valueOf(game.dice.getDice2()));*/
+    }
 }
---
+++ src/main/java/Controller/spaceController.java
@@ -0,0 +1,14 @@
+package Controller;
+
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.text.Text;
+
+import java.io.IOException;
+
+
+public class spaceController extends AnchorPane {
+    @FXML
+    Text hejText;
+}
