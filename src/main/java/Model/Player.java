ab880c10756152b6a29efc6819877e9a0e1567f5
williamProgrammerar <70644042+williamProgrammerar@users.noreply.github.com>
2021-09-29 15:46:29 +0200

    move()

--- src/main/java/Model/Player.java
+++ src/main/java/Model/Player.java
@@ -6,7 +6,8 @@ public class Player {
     private int capital;
     private Piece piece;
     private int pieceID;   // maybe not the best approach, might add unnecessary dependencies
-    private int position; // Temporary until we have fixed tile which should handle this
+    private int position;
+    private boolean hasPassedGo;
 
     public Player(int pieceID) throws Exception {
         this.pieceID = pieceID;
@@ -15,8 +16,32 @@ public class Player {
         this.position = 0;
     }
 
-    public void move(int movement) {
-        piece.getImg().setX(piece.getImg().getX() + movement);
+    /**
+     * move() updates position by adding int spaces to the previous value of position.
+     * if statement checks if the position has exceeded the amount of spaces on the board.
+     * if(true): correct the position according to the board, hasPassedGo = true
+     * else if statement checks if the position is less than space 0 on the board (GO).
+     * else if(true): correct the position according to the board, hasPassedGo = false
+     * @param spaces number of spaces player should move
+     */
+    public void move(int spaces) {
+        //piece.getImg().setX(piece.getImg().getX() + movement);
+        position += spaces;
+        if(position >= 40) {
+            position -= 40;
+            hasPassedGo = true;
+        }
+        else if (position < 0) {
+            position += 40;
+            hasPassedGo = false;
+        }
+        else {
+            hasPassedGo = false;
+        }
+    }
+
+    public void moveTo(int space) {
+        position = space;
     }
 
     public int getCapital() {
