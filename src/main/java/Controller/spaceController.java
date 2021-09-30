56a5853b0dfc76e966670ea560882c88893ae784
rhedinh <rhedinh@student.chalmers.se>
2021-09-30 16:17:04 +0200

    Created all locales in GUI and fixed dice
    Dice is now working and all of the locales is visiable in the GUI.

--- src/main/java/Controller/spaceController.java
+++ src/main/java/Controller/spaceController.java
@@ -1,14 +1,26 @@
 package Controller;
 
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
 import javafx.scene.text.Text;
 
-import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
 
 
-public class spaceController extends AnchorPane {
+public class spaceController {
     @FXML
-    Text hejText;
+    Text localeName;
+    @FXML
+    Rectangle localeColor;
+    @FXML
+    AnchorPane ap;
+
+    private void setOwner(Color c){
+        localeColor.setStroke(c);
+
+    }
 }
