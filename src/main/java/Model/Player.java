43e61348f5cf185410108700e88ba7451abb253d
williamProgrammerar <70644042+williamProgrammerar@users.noreply.github.com>
2021-10-01 14:50:40 +0200

    Made so that whenever player's position is updated, Piece is notified

--- src/main/java/Model/Player.java
+++ src/main/java/Model/Player.java
@@ -3,17 +3,17 @@ package Model;
 import View.Piece;
 
 public class Player {
+    private final Piece piece;
     private int capital;
-    private Piece piece;
     private final int playerId;
     private int position;
     private boolean hasPassedGo;
 
-    public Player(int playerId) {
+    public Player(int playerId) throws Exception {
         this.playerId = playerId;
-        //this.piece = new Piece(pieceID);
         this.capital = 1500;
         this.position = 0;
+        this.piece = new Piece(1, this);
     }
 
     /**
@@ -38,6 +38,7 @@ public class Player {
         else {
             hasPassedGo = false;
         }
+        notifyPiece();
     }
 
     /**
@@ -50,6 +51,7 @@ public class Player {
     public void moveTo(int space, boolean isForwardMovement) {
         hasPassedGo = (space < position) && isForwardMovement;
         position = space;
+        notifyPiece();
     }
 
     public int getPosition() {
@@ -70,5 +72,9 @@ public class Player {
 
     public int getPlayerId() {
         return playerId;
+    }
+
+    public void notifyPiece() {
+        piece.updatePosition();
     }
 }
