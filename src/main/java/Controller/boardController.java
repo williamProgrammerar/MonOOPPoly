15c4937c7dd5c852845f1b50cc78eb5f97d58e44
rhedinh <rhedinh@student.chalmers.se>
2021-10-01 11:32:40 +0200

    Removed hardcoded Locales and will now dynamic initiate
    All of the hardcoded Locales are removed!

--- src/main/java/Controller/boardController.java
+++ src/main/java/Controller/boardController.java
@@ -3,6 +3,7 @@ package Controller;
 import Model.Board;
 import Model.Dice;
 import Model.Game;
+import Model.Space;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
@@ -12,9 +13,7 @@ import javafx.scene.layout.GridPane;
 import javafx.scene.paint.Color;
 
 import java.net.URL;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.ResourceBundle;
+import java.util.*;
 
 
 public class boardController implements Initializable {
@@ -30,80 +29,36 @@ public class boardController implements Initializable {
     ImageView piece = new ImageView(new Image("pic/Asmurf-icon.png"));
     @FXML
     GridPane boardGrid;
-    @FXML
-    spaceController space1Controller;
-    //@FXML spaceController space2Controller;
-    @FXML
-    spaceController space3Controller;
-    @FXML
-    spaceController space6Controller;
-    @FXML
-    spaceController space8Controller;
-    @FXML
-    spaceController space9Controller;
-    @FXML
-    spaceController space11Controller;
-    @FXML
-    spaceController space13Controller;
-    @FXML
-    spaceController space14Controller;
-    @FXML
-    spaceController space16Controller;
-    @FXML
-    spaceController space18Controller;
-    @FXML
-    spaceController space19Controller;
-    @FXML
-    spaceController space21Controller;
-    @FXML
-    spaceController space23Controller;
-    @FXML
-    spaceController space24Controller;
-    @FXML
-    spaceController space26Controller;
-    @FXML
-    spaceController space28Controller;
-    @FXML
-    spaceController space29Controller;
-    @FXML spaceController space31Controller;
-    @FXML spaceController space33Controller;
-    @FXML spaceController space34Controller;
-    @FXML spaceController space37Controller;
-    @FXML spaceController space39Controller;
-    private final List<spaceController> spaceControllers = new ArrayList<>() {{
-        add(space1Controller);
-        add(space3Controller);
-        add(space6Controller);
-        add(space8Controller);
-        add(space9Controller);
-        add(space11Controller);
-        add(space13Controller);
-        add(space14Controller);
-        add(space16Controller);
-        add(space18Controller);
-        add(space19Controller);
-        add(space3Controller);
-        add(space21Controller);
-        add(space23Controller);
-        add(space24Controller);
-        add(space26Controller);
-        add(space28Controller);
-        add(space29Controller);
-        add(space31Controller);
-        add(space33Controller);
-        add(space34Controller);
-        add(space37Controller);
-        add(space39Controller);
-    }};
 
-
-    public boardController() throws Exception {
-    }
+    /*public boardController() throws Exception {
+    }*/
 
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        initLocaleNames();
-        initLocaleColors();
+        updateRecipeList();
+    }
+
+    private void updateRecipeList() {
+        List<Space> spaceList = board.getSpaceList();
+
+        boardGrid.getChildren().clear();
+
+        int r = 10;
+        int c = 10;
+        for (Space s : spaceList) {
+            spaceController i = new spaceController(s, this);
+            boardGrid.add(i, c, r);
+
+            if (r == 10 && c != 0) {
+                c--;
+            } else if (c == 0 && r != 0) {
+                r--;
+            } else if (r == 0 && c != 10) {
+                c++;
+            } else if (c == 10 && r != 10) {
+                r++;
+            }
+        }
     }
 
     public void rollDice() {
@@ -111,36 +66,5 @@ public class boardController implements Initializable {
         piece.setX(piece.getX() + dice.getSum() * 10);
         dice1.setText(String.valueOf(dice.getDice1()));
         dice2.setText(String.valueOf(dice.getDice2()));
-    }
-
-    private void initLocaleNames() {
-        space1Controller.localeName.setText(board.getSpace(1).getSpaceName());
-        space3Controller.localeName.setText(board.getSpace(3).getSpaceName());
-        space6Controller.localeName.setText(board.getSpace(6).getSpaceName());
-        space8Controller.localeName.setText(board.getSpace(8).getSpaceName());
-        space9Controller.localeName.setText(board.getSpace(9).getSpaceName());
-        space21Controller.localeName.setText(board.getSpace(22).getSpaceName());
-        space23Controller.localeName.setText(board.getSpace(24).getSpaceName());
-        space24Controller.localeName.setText(board.getSpace(25).getSpaceName());
-        space26Controller.localeName.setText(board.getSpace(27).getSpaceName());
-        space28Controller.localeName.setText(board.getSpace(29).getSpaceName());
-        space29Controller.localeName.setText(board.getSpace(30).getSpaceName());
-        if (space11Controller.ap.getRotate() != 0) {
-            space11Controller.localeName.setRotate(-90);
-        }
-    }
-
-    private void initLocaleColors() {
-        space1Controller.localeColor.setFill(Color.web("#FA6601"));
-        space3Controller.localeColor.setFill(Color.web("#FA6601"));
-        space6Controller.localeColor.setFill(Color.BROWN);
-        space8Controller.localeColor.setFill(Color.BROWN);
-        space9Controller.localeColor.setFill(Color.BROWN);
-        space21Controller.localeColor.setFill(Color.GREEN);
-        space23Controller.localeColor.setFill(Color.GREEN);
-        space24Controller.localeColor.setFill(Color.GREEN);
-        space26Controller.localeColor.setFill(Color.DARKGRAY);
-        space28Controller.localeColor.setFill(Color.DARKGRAY);
-        space29Controller.localeColor.setFill(Color.DARKGRAY);
     }
 }
--- src/main/java/Controller/spaceController.java
+++ src/main/java/Controller/spaceController.java
@@ -1,26 +1,54 @@
 package Controller;
 
+import Model.Locale;
+import Model.Space;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
 import javafx.scene.text.Text;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
 
-public class spaceController {
+public class spaceController extends AnchorPane{
     @FXML
     Text localeName;
     @FXML
     Rectangle localeColor;
     @FXML
-    AnchorPane ap;
+    Rectangle spaceTile;
 
-    private void setOwner(Color c){
+    private Space space;
+
+    public spaceController(Space space, boardController boardController) {
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/fxml/MonopolySpace.fxml"));
+        fxmlLoader.setRoot(this);
+        fxmlLoader.setController(this);
+
+        try {
+            fxmlLoader.load();
+        } catch (IOException exception) {
+            throw new RuntimeException(exception);
+        }
+
+        this.space = space;
+        //this.parentController = recipeSearchController;
+        localeName.setText(space.getSpaceName());
+    }
+/*
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        localeColor.setStroke(Color.BLACK);
+        spaceTile.setStroke(Color.BLACK);
+    }*/
+
+    private void setOwner(Color c) {
         localeColor.setStroke(c);
-
+        spaceTile.setStroke(c);
     }
 }
