21f5efa532aa38910974b909d4a4cb63980491e8
williamProgrammerar <70644042+williamProgrammerar@users.noreply.github.com>
2021-10-03 17:09:18 +0200

    Created a new Piece class

---
+++ src/main/java/View/Piece.java
@@ -0,0 +1,23 @@
+package View;
+
+import Model.Player;
+import javafx.scene.image.ImageView;
+
+public class Piece {
+
+    private final ImageView piece;
+    private final Player player;
+
+    public Piece(ImageView piece, Player player) {
+        this.piece = piece;
+        this.player = player;
+    }
+
+    public ImageView getPiece() {
+        return piece;
+    }
+
+    public Player getPlayer() {
+        return player;
+    }
+}
