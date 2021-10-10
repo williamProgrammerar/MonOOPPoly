499dc9a285e08dce1e4d5c5b7d3f19488e4e85fd
williamProgrammerar <70644042+williamProgrammerar@users.noreply.github.com>
2021-10-10 21:48:54 +0200

    Comments for piececontroller

--- src/main/java/Controller/PieceController.java
+++ src/main/java/Controller/PieceController.java
@@ -12,6 +12,9 @@ public class PieceController {
     private final List<Image> images = new ArrayList<>();
     private final Random rand = new Random();
 
+    /**
+     * Constructor calls initImages to initiate the list of pieces.
+     */
     public PieceController() {
         initImages();
     }
@@ -20,6 +23,10 @@ public class PieceController {
         return new ImageView(selectPiece());
     }
 
+    /**
+     * Randomly selects a piece from the images list and then removes it so the same piece doesn't get selected again.
+     * @return returns a random image from images list
+     */
     private Image selectPiece() {
         int i = rand.nextInt(images.size());
         Image img =  images.get(i);
