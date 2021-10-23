94ea784dd2d0f7157c3ac9b27585dc34fca281f6
williamProgrammerar <70644042+williamProgrammerar@users.noreply.github.com>
2021-10-23 13:06:27 +0200

    Small refactoring to make code more consistent.

--- src/main/java/Model/House.java
+++ src/main/java/Model/House.java
@@ -1,11 +1,10 @@
 package Model;
-/**
- * @author JonEmilsson
- */
 
 /**
  * Empty class that just makes it easy to know that it's an object being created, also
  * allows for extension if house needs behaviour.
+ *
+ * @author JonEmilsson
  */
 public class House {
 
