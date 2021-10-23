85535cb4936c211b709808d7891241790b6b81c4
williamProgrammerar <70644042+williamProgrammerar@users.noreply.github.com>
2021-10-23 13:10:57 +0200

    Small refactoring to make code more consistent.

--- src/main/java/Model/Space.java
+++ src/main/java/Model/Space.java
@@ -2,7 +2,6 @@ package Model;
 
 public class Space {
     private final String spaceName;
-    //private final int tile;
 
     public Space(String spaceName) {
         this.spaceName = spaceName;
