e2cfa70366cdca3759e6846ce125388d2845e3d6
williamProgrammerar <70644042+williamProgrammerar@users.noreply.github.com>
2021-10-23 12:55:06 +0200

    Small refactoring to make code more consistent.

--- src/main/java/Model/ChanceCardCreator.java
+++ src/main/java/Model/ChanceCardCreator.java
@@ -6,6 +6,7 @@ import java.util.Random;
 
 public class ChanceCardCreator {
     private final IChanceCard chanceCard;
+
     private static final List<IChanceCard> chanceCards = new ArrayList<>() {{
         add(new MoneyChanceCard("Du måste betala kåravgift, betala 240 kr till kåren.", -240));
         add(new MoneyChanceCard("Tack för att du deltog i kursutvärderingen, inkassera 100 kr.", 100));
@@ -17,7 +18,7 @@ public class ChanceCardCreator {
         this.chanceCard = chanceCards.get(cardNumber);
     }
 
-    public IChanceCard getChanceCard(){
+    public IChanceCard getChanceCard() {
         return chanceCard;
     }
 }
