c49a33d82b4316530d343b74847ed64990e58931
rhedinh <rhedinh@student.chalmers.se>
2021-10-24 16:01:42 +0200

    MoneyChanceCardTest
    If it's a negative moneychancecard, does it takes the right amount of money from player

--- src/test/java/modelTests/MoneyChanceCardTest.java
+++ src/test/java/modelTests/MoneyChanceCardTest.java
@@ -7,11 +7,20 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class MoneyChanceCardTest {
     @Test
-    void testMoneyChanceCard() {
+    void testPositiveMoneyChanceCard() {
         MoneyChanceCard moneyChanceCard = new MoneyChanceCard("Text", 500);
         Player player = new Player(1, 1500);
 
         moneyChanceCard.doAction(player);
         assertEquals(player.getCapital(), 2000);
+    }
+
+    @Test
+    void testNegativeMoneyChanceCard(){
+        MoneyChanceCard moneyChanceCard = new MoneyChanceCard("Text", -500);
+        Player player = new Player(1, 1500);
+
+        moneyChanceCard.doAction(player);
+        assertEquals(player.getCapital(), 1000);
     }
 }
