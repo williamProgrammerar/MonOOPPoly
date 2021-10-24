4391aff5341bd81ec6a3fe7d339f9cce6b2581ae
williamProgrammerar <70644042+williamProgrammerar@users.noreply.github.com>
2021-10-24 12:06:31 +0200

    Game tests

--- src/test/java/modelTests/GameTest.java
+++ src/test/java/modelTests/GameTest.java
@@ -1,36 +1,29 @@
 package modelTests;
 
 import model.*;
+import observers.Observer;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 public class GameTest {
-    private GameSettings gameSettings;
     private Game game;
 
     @BeforeEach
     void initGame() {
-        gameSettings = new GameSettings();
+        GameSettings gameSettings = new GameSettings();
         Player player = new Player(1,1500);
         Player player2 = new Player(2, 1500);
+        Player player3 = new Player(3, 1500);
         gameSettings.getPlayers().add(player);
         gameSettings.getPlayers().add(player2);
+        gameSettings.getPlayers().add(player3);
         game = new Game(gameSettings);
         game.getDice().rollDice();
+        Observer observer = () -> System.out.println("hej");
+        game.attach(observer);
     }
-
-    /*
-    @Test
-    void moveTest() {
-        game.move(0);
-        int previousPos = game.getCurrentPlayer().getPosition();
-        Space previousSpace = game.getCurrentSpace();
-        game.move(game.getDice().getSum());
-        assertNotEquals(previousPos, game.getCurrentPlayer().getPosition());
-        assertNotEquals(previousSpace, game.getCurrentSpace());
-    }*/
 
     @Test
     void nextPlayerTest() {
@@ -66,6 +59,16 @@ public class GameTest {
     }
 
     @Test
+    void movePastGo() {
+        game.getCurrentPlayer().moveTo(39,true);
+        assertFalse(game.getCurrentPlayer().HasPassedGo());
+
+        game.move(10);
+        assertTrue(game.getCurrentPlayer().HasPassedGo());
+    }
+
+
+    @Test
     void testJailTurn() {
 
     }
@@ -82,5 +85,51 @@ public class GameTest {
             game.endTurn();
         }
         assertTrue(game.getPlayers().get(game.getPlayers().size() - 1).isBankrupt());
+    }
+
+    @Test
+    void testBuyHouse() {
+        Locale locale1 = (Locale) game.getBoard().getSpace(game.getBoard().findSpace("WINDEN"));
+        Locale locale2 = (Locale) game.getBoard().getSpace(game.getBoard().findSpace("XP-LABBET"));
+        Locale locale3 = (Locale) game.getBoard().getSpace(game.getBoard().findSpace("BULTEN"));
+
+        game.buyHouse(locale1);
+        assertEquals(locale1.getAmountOfHouses(), 0);
+
+        game.getCurrentPlayer().getProperties().add(locale1);
+        game.getCurrentPlayer().getProperties().add(locale2);
+        game.getCurrentPlayer().getProperties().add(locale3);
+
+        game.buyHouse(locale1);
+        assertEquals(locale1.getAmountOfHouses(), 1);
+    }
+
+    @Test
+    void testNextBankrupt() {
+       game.getPlayers().get(1).setBankrupt();
+
+       game.next();
+
+       assertSame(game.getPlayers().get(0), game.getCurrentPlayer());
+    }
+
+    @Test
+    void testSelectSpace() {
+        game.move(5);
+        game.setSelectedSpace(game.getCurrentSpace());
+        Space selectedSpace = game.getSelectedSpace();
+        game.setSelectedSpace(selectedSpace);
+        assertSame(game.getCurrentSpace(), game.getSelectedSpace());
+    }
+
+    @Test
+    void getPlayerUsingIDTest() throws Exception {
+        Player playerTest = game.getPlayerUsingID(1);
+        assertSame(playerTest, game.getCurrentPlayer());
+        try {
+            Player playerTest2 = game.getPlayerUsingID(5);
+        } catch (Exception e) {
+            System.out.println("Player doesn't exist");
+        }
     }
 }
