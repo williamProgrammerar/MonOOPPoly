e9dadf39da32441327adbaf146c9c968477f86cc
williamProgrammerar <70644042+williamProgrammerar@users.noreply.github.com>
2022-01-21 16:25:36 +0100

    Added comments
    Co-Authored-By: rhedinh <70346599+rhedinh@users.noreply.github.com>

--- src/main/java/model/IItem.java
+++ src/main/java/model/IItem.java
@@ -1,7 +1,16 @@
 package model;
 
+/**
+ * An interface for items of the TradeController.
+ *
+ * @param <T> The owner of the item, for example a player or a property
+ */
 public interface IItem<T> {
     public String getName();
 
+    /**
+     * Returns an item of type T.
+     * @return item.
+     */
     public T getItemOwner();
 }
